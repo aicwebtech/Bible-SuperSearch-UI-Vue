0f0538120b871ebba473def12d46f6482f53f4e0
AIC Web Tech <pwm@preservedword.com>
2025-09-27 22:40:25 -0400

    Architecture, cont

--- src/main.js
+++ src/main.js
@@ -1,11 +1,52 @@
-import './assets/main.css'
+import './assets/main.css';
+import defaultConfig from './config-default.js';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { createVuetify } from 'vuetify';
+import App from './App.vue';
+// /import vue from 'eslint-plugin-vue';
 
-const app = createApp(App)
+var app_config = typeof window !== 'undefined' && window.app_config ? window.app_config : null;
 
-app.use(createPinia())
+if (typeof app_config !== 'object' || !app_config) {
+    app_config = {};
+}
 
-app.mount('#app')
+var merged_config = { ...defaultConfig, ...app_config };
+
+console.log('Merged config:', merged_config);
+
+const vuetify = createVuetify({
+    theme: {
+        defaultTheme: 'light',
+    },
+});
+
+// const app = createApp(App);
+// app.use(vuetify);
+// app.use(createPinia());
+// app.provide('config', merged_config);
+// app.mount('#app');
+
+// const extra = createApp(App);
+// extra.use(vuetify);
+// extra.use(createPinia());
+// extra.provide('config', merged_config);
+// extra.mount('#extra');
+
+createBibleSuperSearchApp('#app', merged_config);
+createBibleSuperSearchApp('#extra', merged_config);
+
+function createBibleSuperSearchApp(mount, config) {
+    mount = mount || '#app';
+    var app2 = createApp(App);
+
+    app2.use(vuetify);
+    app2.use(createPinia());
+    app2.provide('config', config);
+
+    app2.mount(mount);
+}
+
+export { createBibleSuperSearchApp };
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue(), vueDevTools()],
+    base: './',
+    build: {
+        rollupOptions: {
+            // https://rollupjs.org/configuration-options/#output-chunkfilenames
+            output: {
+                entryFileNames: `assets/[name].js`, // default is [name].js
+                chunkFileNames: `assets/[name].js`, // default is [name]-[hash].js
+                assetFileNames: `assets/[name].[ext]`, // default is [name]-[hash].[ext]
+            },
+        },
     },
-  },
-})
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+});
