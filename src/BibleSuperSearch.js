f6e37e46e428dcfd3b13e64bb66b84d9e1fb71a6
AIC Web Tech <pwm@preservedword.com>
2025-10-03 11:45:16 -0400

    Loading statics

--- src/BibleSuperSearch.js
+++ src/BibleSuperSearch.js
@@ -4,6 +4,7 @@ import defaultConfig from './config-default.js';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { createVuetify } from 'vuetify';
+import axios from 'axios';
 import App from './App.vue';
 // /import vue from 'eslint-plugin-vue';
 
@@ -23,6 +24,11 @@ const vuetify = createVuetify({
 var statics = null;
 var target = null;
 
+if (typeof window.biblesupersearch_statics === 'object') {
+    statics = Object.assign({}, window.biblesupersearch_statics);
+    console.log('Using biblesupersearch_statics:', statics);
+}
+
 if (
     typeof window.biblesupersearch_config_options === 'object' &&
     window.biblesupersearch_config_options
@@ -39,11 +45,33 @@ console.log('Instances', window.biblesupersearch_instances);
 // createBibleSuperSearchApp('#extra', merged_config);
 
 function createBibleSuperSearchApp(mount, config) {
+    if (statics === null) {
+        // try to load statics from server
+        var url = (config.apiUrl || '') + '/api/statics';
+        console.log('Loading statics from', url);
+        axios
+            .get(url)
+            .then((response) => {
+                statics = response.data.results;
+                console.log('Loaded statics:', statics);
+
+                createBibleSuperSearchApp(mount, config);
+            })
+            .catch((error) => {
+                // Todo: show error to user, along with backup Bibles to download
+                console.error('Error loading statics:', error);
+                statics = {};
+            });
+
+        return;
+    }
+
     mount = mount ? '#' + mount : '#biblesupersearch_container';
     var app = createApp(App);
 
     app.use(vuetify);
     app.use(createPinia());
+
     app.provide('config', config);
     app.provide('statics', statics);
 
